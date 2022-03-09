53c9c83bcd823b658f16e7eb5b9378d1b9a2c090
Dhairya <dhairyashelar1@gmail+654+654165+4*/.com ~+*/>
2022-03-09 12:09:45 +0530

    - First Commit.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/budget-book'),
+      dir: require('path').join(__dirname, './coverage/movie-mania'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'budget-book'`, () => {
+  it(`should have as title 'movie-mania'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('budget-book');
+    expect(app.title).toEqual('movie-mania');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('budget-book app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('movie-mania app is running!');
   });
 });
