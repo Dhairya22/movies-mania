956a29804398428deb1fa7097228415cb8fe26ee
Dhairya <dhairyashelar1@gmail+654+654165+4*/.com ~+*/>
2022-03-09 15:18:58 +0530

    UI Changes

--- src/app/component/movie-list/movie-list.component.ts
+++ src/app/component/movie-list/movie-list.component.ts
@@ -2,59 +2,105 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+interface category {
+    name: string;
+}
+
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.scss'],
+    selector: 'app-movie-list',
+    templateUrl: './movie-list.component.html',
+    styleUrls: ['./movie-list.component.scss'],
 })
 export class MovieListComponent implements OnInit {
+    movieForm!: FormGroup;
+    movieList: Array<any> = [];
+    imgPath: any;
+    showError: Boolean = false;
+    errorMsg = '';
+    imbd: any;
+    category: Array<category> = [
+        { name: 'Comedy' },
+        { name: 'Thriller' },
+        { name: 'Horror' },
+        { name: 'Action' },
+        { name: 'Sci-Fi' },
+    ];
+    selectCategory: any;
 
-  movieForm!: FormGroup;
-  movieList: Array<any> = [];
-  imgPath: any;
-  showError: Boolean = false;
-  errorMsg = "";
+    constructor(private http: HttpClient) { }
 
-  imbd: any;
+    ngOnInit(): void {
+        this.imbd = 'src/assets/imbd.jpg';
+        this.movieForm = new FormGroup({
+            movieName: new FormControl('', Validators.required),
+        });
 
-  constructor(private http: HttpClient) {}
+        const checkbox: any = document.getElementById('checkbox');
 
-  ngOnInit(): void {
-      this.imbd = "src/assets/imbd.jpg";
-    this.movieForm = new FormGroup({
-      movieName: new FormControl('', Validators.required),
-    });
+        checkbox.addEventListener('change', () => {
+            document.body.classList.toggle('dark');
+        });
+    }
 
-    const checkbox: any = document.getElementById('checkbox');
+    onSubmit() {
+        const { movieName } = this.movieForm.getRawValue();
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=f6bb9f69072f0d231a0f04b55854335a&language=en-US
+                      &query=${movieName}&page=1&include_adult=false`;
+        this.http.get(url).subscribe(
+            (response: any) => {
+                // this.showError = false;
+                if (response && response.total_results != 0) {
+                    this.showError = false;
+                    this.movieList = response?.results;
+                    this.movieList.map((items) => {
+                        this.imgPath = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${items.poster_path}`;
+                    });
+                } else {
+                    this.movieList = [];
+                    this.showError = true;
+                    this.errorMsg = 'No records found.';
+                }
+            },
+            (error) => {
+                if (error) {
+                    this.movieList = [];
+                    this.showError = true;
+                    this.errorMsg = 'Please enter a Movie name.';
+                }
+            }
+        );
+    }
 
-    checkbox.addEventListener('change', ()=>{
-  document.body.classList.toggle('dark');
-})
-  }
+    selectedCategory(category: any) {
+        this.selectCategory = category.name;
+        this.movieForm.patchValue({
+            movieName: this.selectCategory,
+        });
 
-  onSubmit() {
-    const { movieName } = this.movieForm.getRawValue();
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=f6bb9f69072f0d231a0f04b55854335a&language=en-US
-                &query=${movieName}&page=1&include_adult=false`;
-    this.http.get(url).subscribe((response: any) => {
-        // this.showError = false;
-        if(response && response.total_results != 0){
-            this.showError = false;
-            this.movieList = response?.results;
-            this.movieList.map(items => {
-                this.imgPath = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${items.poster_path}`;
-            })
-        }else {
-            this.movieList = [];
-            this.showError = true;
-            this.errorMsg = "No records found.";
-        }
-    },(error) => {
-        if(error){
-            this.movieList = [];
-            this.showError = true;
-            this.errorMsg = "Please enter a Movie name.";
-        }
-    });
-  }
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=f6bb9f69072f0d231a0f04b55854335a&language=en-US
+                      &query=${this.selectCategory}&page=1&include_adult=false`;
+        this.http.get(url).subscribe(
+            (response: any) => {
+                // this.showError = false;
+                if (response && response.total_results != 0) {
+                    this.showError = false;
+                    this.movieList = response?.results;
+                    this.movieList.map((items) => {
+                        this.imgPath = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${items.poster_path}`;
+                    });
+                } else {
+                    this.movieList = [];
+                    this.showError = true;
+                    this.errorMsg = 'No records found.';
+                }
+            },
+            (error) => {
+                if (error) {
+                    this.movieList = [];
+                    this.showError = true;
+                    this.errorMsg = 'Please enter a Movie name.';
+                }
+            }
+        );
+    }
 }
